a6961513c83be11aaa5c1c6b7413fe67346e934c
Mai El-Zein <mai_ez@outlook.dk>
2025-02-20 18:52:39 +0100

    yes

--- src/pages/todo/todo.js
+++ src/pages/todo/todo.js
@@ -1,17 +1,4 @@
 window.addEventListener("load", resetPage);
-// Oprette en ny opgave med et unikt ID og en beskrivelse.
-// Gemme mere end en simpel streng for hver opgave for at øge kompleksiteten. Dette kunne være kvantitet
-// (antal) eller anden relevant information.
-
-// Tillade brugerne at markere opgaver som "færdige", hvorefter de flyttes til en "Færdig"-liste.
-// Tillade brugerne at fortryde færdiggørelsen af en opgave, så den ryger tilbage til "ToDo"-listen.
-// Tillade brugerne at slette opgaver.
-
-// Evt. ekstra funktioner
-// Brug localStorage til at gemme opgaverne. Når en bruger opretter en ny opgave,
-// sletter en opgave, eller ændrer status for en opgave, skal disse ændringer gemmes i localStorage.
-//  Når brugeren besøger appen igen, skal opgaverne hentes fra localStorage, så de stadig kan se deres opgaveliste,
-// selv efter at de har lukket og genåbnet browseren.
 
 // Global Arrays
 
